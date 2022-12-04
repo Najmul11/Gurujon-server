1688fa6a3f38f4e68bcd1d93a22ad30ffe6c7d01
NazPro <najmulbinnurul3@gmail.com>
2022-12-04 17:31:35 +0600

    server ready

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const app=express()
 const port= process.env.PORT || 5000 ;
 
-
+app.get('/', (req, res)=>{
+    res.send('GURUJON BACKEND CONNECTED')
+})
 
 app.listen(port, (req, res)=>{
     console.log('listening to port ',port);
