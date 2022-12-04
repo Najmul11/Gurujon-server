15c9649547c920c3981766b6dd89b2b7ab284779
NazPro <najmulbinnurul3@gmail.com>
2022-12-04 23:42:50 +0600

    data added

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 
+
 const app=express()
 const port= process.env.PORT || 5000 ;
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send('GURUJON BACKEND CONNECTED')
 })
+
+
+const courseCategories=require('./Data/course-categories.json')
+app.get('/categories', (req, res)=>{
+    res.send(courseCategories)
+})
+
 
 app.listen(port, (req, res)=>{
     console.log('listening to port ',port);
